d1e06c80abe1f953083df307b1b48183bd976226
dtwan0212 <toando0212@gmail.com>
2023-09-15 20:13:57 +0700

    sdhfjhdgs

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ document.addEventListener('DOMContentLoaded', function() {
     var min = document.querySelector('.min');
     var second = document.querySelector('.second');
   
-    var startDate = new Date(2023, 02, 16);
-    days.innerText = Math.floor((new Date - startDate)/86400000);
-    countTime();
+    // var startDate = new Date(2023, 02, 16);
+    // days.innerText = Math.floor((new Date - startDate)/86400000);
+    // countTime();
   
-    function countTime() {
-      let today = new Date();
-      let ms = (today - startDate) % 86400000;
-      hour.innerText = Math.floor(ms / 3600000);
-      min.innerText = Math.floor(ms % 3600000 / 60000);
-      second.innerText = Math.floor(ms % 3600000 % 60000 / 1000);
-    }
+    // function countTime() {
+    //   let today = new Date();
+    //   let ms = (today - startDate) % 86400000;
+    //   hour.innerText = Math.floor(ms / 3600000);
+    //   min.innerText = Math.floor(ms % 3600000 / 60000);
+    //   second.innerText = Math.floor(ms % 3600000 % 60000 / 1000);
+    // }
   
     setInterval(countTime, 1000);
   
